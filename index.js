c74b6efd81bd884f81d41aea9c6982988b2be6db
Michał Nowacki <mikinowacki@gmail.com>
2016-09-20 21:21:40 +0200

    simplifying, update

--- index.js
+++ index.js
@@ -14,93 +14,71 @@ exports.ATSDSocketClient = ATSDSocketClient;
  * @param {String} port
  */
 
-var client = undefined;
 function ATSDSocketClient(port, atsdUrl) {
     this._atsdUrl = atsdUrl;
     this._port = port;
-    client = new Socket();
-    client.on('data', function (data){
+    this.client = new Socket();
+    this.client.on('data', function (data){
         console.log(data.toString());
+    });
+
+    this.client.on('connect', function () {
+        //this.client.setKeepAlive(true,1000);
+        console.log('server: STARTUP -- AxiBase: Connected to ' + atsdUrl + ' on port ' + port);
+    });
+
+    this.client.on('error', function(err) {
+        console.log('Axibase: Socket error:', err.message);
+    });
+
+    this.client.on('close', function(err) {
+        console.log('Axibase: Socket close:', err.message);
+    });
+
+    this.client.on('timeout', function(err) {
+        console.log('Axibase: Socket error:', err.message);
     });
 }
 
 
 
+
+
 ATSDSocketClient.prototype.connect = function (cb) {
-    client.connect(this._port, this._atsdUrl, function () {
+    this.client.connect(this._port, this._atsdUrl, function () {
         console.log('Connected To AxiBase');
-        cb();
+        if(cb) {
+            cb();
+        }
     });
 };
 
 ATSDSocketClient.prototype.write = function (message) {
 
     var that = this;
-    if(message.mime_type === 'metric/timeseries' && message.metrics){
-        var insertTemp = [];
-        var metricsGrouped = _.groupBy(message.metrics, function(met){return met.timeseries[0][0]});
-        var timestamps = Object.keys(metricsGrouped);
-        timestamps.forEach(function (timestamp) {
-            that.parseMessage(timestamps[timestamp]).forEach(function (singleMetric) {
-                insertTemp.push(singleMetric.trim());
+    var tags = message.tags;
+    var metrics = message.metrics;
+    var messageBase = 'series e:agent ';
+    if(metrics){
+        metrics.forEach(function (metric) {
+            var metricName = metric.metric_name;
+            var metricMessageBase = messageBase + parseTags(_.defaults(metric.tags, tags));
+            metric.timeseries.forEach(function(single){
+                var messageToWrite = metricMessageBase + parseMetricValues(metricName, single) + '\n';
+                that.insertData(messageToWrite);
             });
         });
-        that.insertData(insertTemp.join("\n"), client);
     }
-
-
-     /*   if (typeof message.metrics === 'object' && message.length) {
-            var insertTemp = [];
-            message.metrics.forEach(function (metric) {
-                that.parseMessage(metric).forEach(function (singleMetric) {
-                    insertTemp.push(singleMetric.trim());
-                });
-            });
-            that.insertData(insertTemp.join("\n"), client);
-        } else if (typeof message.metrics === 'object') {
-            var insertTemp = [];
-            that.parseMessage(message.metrics).forEach(function (singleMetric) {
-                insertTemp.push(singleMetric.trim());
-            });
-            that.insertData(insertTemp.join("\n"), client);
-        } else if (message.indexOf('series') > -1) {
-            that.insertData(message, client);
-        } else {
-            console.error('Not supported type');
-            return false;
-        }
-*/
 };
 
 
 
-ATSDSocketClient.prototype.insertData = function (metric, client) {
-    client.write('debug '+ metric, function(){
-        console.log('metric:','debug '+ metric, 'is written');
-    });
-    //client.end();
+ATSDSocketClient.prototype.insertData = function (metric) {
+    this.client.write(metric);
 };
 
 ATSDSocketClient.prototype.end = function () {
     this.client.end();
-};
-
-ATSDSocketClient.prototype.parseMessage = function (message) {
-    var metricArray = [], parsedString = 'series ';
-
-    if (message.entity) {
-        parsedString = parsedString.concat('e:' + message.entity + ' ');
-    } else {
-        parsedString = parsedString.concat('e:agent ');
-    }
-
-    if (message.tags) {
-        parsedString = parsedString.concat(parseTags(message.tags));
-    }
-    if (message.data) {
-        parseMetric(message.metric, message.data, parsedString, metricArray);
-    }
-    return metricArray;
 };
 
 function parseTags(tags) {
@@ -111,35 +89,10 @@ function parseTags(tags) {
     return tempString;
 }
 
-function parseMetric(metricName, data, parsedString, metricArray) {
-    if (data.length) {
-        data.forEach(function (value) {
-            metricArray.push(parsedString + parseMetricValues(metricName, value));
-        });
-    } else if (typeof data === 'object') {
-        metricArray.push(parsedString + parseMetricValues(metricName, data));
-    }
-}
-
 function parseMetricValues(metricName, value) {
-    if (value.d) {
-        return 's:' + toTimestamp(value.d) + ' ' + 'm:' + metricName.trim() + '=' + value.v + ' ';
-    } else if (value.s) {
-        return 's:' + value.s + ' ' + 'm:' + metricName + '=' + value.v + ' ';
-    } else if (value.ms) {
-        return 'ms:' + value.ms + ' ' + 'm:' + metricName + '=' + value.v + ' ';
+    if (value[0]) {
+        return 's:' + value[0] + ' ' + 'm:' + metricName.trim() + '=' + value[1] + ' ';
     }
 }
-
-function toTimestamp(strDate) {
-    var datum = Date.parse(strDate);
-    return datum/1000;
-}
-
-/*
-
- */
-
-
 
 
