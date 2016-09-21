bf05b0468f54f8fdb7435d1e2ac8d64de5004fb1
Michał Nowacki <mikinowacki@gmail.com>
2016-09-21 14:34:01 +0200

    simplifying, update

--- index.js
+++ index.js
@@ -14,85 +14,62 @@ exports.ATSDSocketClient = ATSDSocketClient;
  * @param {String} port
  */
 
-function ATSDSocketClient(port, atsdUrl) {
+function ATSDSocketClient(port, atsdUrl, logSystem) {
+    if(!logSystem){
+        logSystem = console;
+    }
+    if(!port){
+        return {error: 'Missind required paramter: ATSD PORT'};
+    }
+    if(!atsdUrl){
+        return {error: 'Missind required paramter: ATSD HOST'};
+    }
+
     this._atsdUrl = atsdUrl;
     this._port = port;
+    this.count = 0;
     this.client = new Socket();
     this.client.on('data', function (data){
-        console.log(data.toString());
+        logSystem.log(data.toString());
     });
 
     this.client.on('connect', function () {
         //this.client.setKeepAlive(true,1000);
-        console.log('server: STARTUP -- AxiBase: Connected to ' + atsdUrl + ' on port ' + port);
+        logSystem.log('server: STARTUP -- AxiBase: Connected to ' + atsdUrl + ' on port ' + port);
     });
 
     this.client.on('error', function(err) {
-        console.log('Axibase: Socket error:', err.message);
+        logSystem.error('Axibase: Socket error:', err.message);
     });
 
     this.client.on('close', function(err) {
-        console.log('Axibase: Socket close:', err.message);
+        logSystem.log('Axibase: Socket close:', err.message);
+        logSystem.log('Trying to reconnect');
+        this.connect();
     });
 
     this.client.on('timeout', function(err) {
-        console.log('Axibase: Socket error:', err.message);
+        logSystem.error('Axibase: Socket error:', err.message);
+        logSystem.log('Trying to reconnect');
+        this.connect();
     });
 }
 
-
-
-
-
 ATSDSocketClient.prototype.connect = function (cb) {
     this.client.connect(this._port, this._atsdUrl, function () {
-        console.log('Connected To AxiBase');
         if(cb) {
             cb();
         }
     });
 };
 
-ATSDSocketClient.prototype.write = function (message) {
-
-    var that = this;
-    var tags = message.tags;
-    var metrics = message.metrics;
-    var messageBase = 'series e:agent ';
-    if(metrics){
-        metrics.forEach(function (metric) {
-            var metricName = metric.metric_name;
-            var metricMessageBase = messageBase + parseTags(_.defaults(metric.tags, tags));
-            metric.timeseries.forEach(function(single){
-                var messageToWrite = metricMessageBase + parseMetricValues(metricName, single) + '\n';
-                that.insertData(messageToWrite);
-            });
-        });
-    }
-};
-
-
-
-ATSDSocketClient.prototype.insertData = function (metric) {
+ATSDSocketClient.prototype.write = function (metric) {
     this.client.write(metric);
+    this.count++;
 };
 
 ATSDSocketClient.prototype.end = function () {
     this.client.end();
 };
-
-function parseTags(tags) {
-    var keys = Object.keys(tags), tempString = '';
-    keys.forEach(function (key) {
-        tempString = tempString.concat('t:' + key + '=' + tags[key] + ' ');
-    });
-    return tempString;
-}
-
-function parseMetricValues(metricName, value) {
-    if (value[0]) {
-        return 's:' + value[0] + ' ' + 'm:' + metricName.trim() + '=' + value[1] + ' ';
-    }
-}
 
 
